a3f1b4e30271a8b7379b5b5043a4d4501a2ec296
n1c0145 <nicolas.lozalml@gmail.com>
2025-09-30 17:36:48 -0500

    use routing outlet to father and children routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { HomeComponent } from "./main/home/home.component";
 
 export const routes: Routes = [
   {
@@ -10,5 +12,13 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: '',
+    component: HeaderComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+
+    ]
   }
 ];
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule, RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  title = 'Service Desk';
+  @ViewChild('sidenav') sidenav!: MatSidenav;
 
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
 }
