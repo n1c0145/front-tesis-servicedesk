16a6314e0d5b3d851162856b2855610ca1b2a9cb
n1c0145 <nicolas.lozalml@gmail.com>
2025-10-03 18:06:28 -0500

    add logout method and button

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -4,7 +4,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 
 
 @Component({
@@ -22,7 +22,13 @@ export class HeaderComponent {
   title = 'Service Desk';
   @ViewChild('sidenav') sidenav!: MatSidenav;
 
+  constructor(private router: Router) { }
+
   toggleSidenav() {
     this.sidenav.toggle();
+  }
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
   }
 }
